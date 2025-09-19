200d4e5e32af246cc689327cb327cd8010943040
Bartek150 <madaneckib@gmail.com>
2025-09-19 18:26:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import random
+letters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+#r = ["+","-","/","*","!","&","$","#","?","=","@","a","b","c","d","e","f","g","h","i","j","k","l","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0"]
+
+password = ""
+for i in range(10):
+    letter = random.choice(letters)
+    password += letter
+print("twoje hasło to " + password)
 
